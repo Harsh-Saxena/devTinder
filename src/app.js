7f3eea272255b57cc22210807f2c812f4fefa403
Harsh Saxena <hsaxena147@gmail.com>
2025-10-05 00:17:53 +0530

    data sanitization and validation

--- src/app.js
+++ src/app.js
@@ -1,21 +1,78 @@
 const express = require("express");
 const User = require("./models/user");
+
 const app = express();
 const { connectDB } = require("./config/database");
 
-
 app.use(express.json()); // Middleware to parse JSON request bodies for all apis
 
+//Post data to create user
 app.post("/signup", async (req, res) => {
   //Creating instance of user model
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("User added succsessfully");
   } catch (err) {
+    console.log("Error in creating user", err.message);
+    res.status(400).send(err.message);
+  }
+});
+//Find user by emailID
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailID;
+  try {
+    const user = await User.find({ emailID: userEmail });
+    if (user.length === 0) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
     console.log("Error in creating user", err);
   }
+});
+//Get all user from database
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.send(user);
+  } catch (err) {
+    console.log("Error in creating user", err);
+  }
+});
+
+//delete user api
+app.delete("/deleteUser", async (req, res) => {
+  const userID = req.body.userID;
+  try {
+    const deleteUser = await User.findByIdAndDelete(userID);
+    res.send("User deleted successfully");
+  } catch (err) {
+    console.log("Error in deleting user", err);
+  }
+});
+
+//Update the User
+app.patch("/updateUser", async (req, res) => {
+    const userID = req.body.userID;
+    const data = req.body;
+
+    const ALLOWED_UPDATES = ['firstName','lastName','password'];
+
+    try{
+    const isUpdateAllowed = Object.keys(data).every((key) => {
+        ALLOWED_UPDATES.includes(key);
+    });
+
+    if(!isUpdateAllowed){
+        return res.status(400).send("Update is not allowed");
+    }
+        const updateUser = await User.findByIdAndUpdate(userID, data);
+        res.send("User updated successfully");  
+    }catch(err){
+        console.log("Error in updating user", err);
+    }
 });
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,33 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 30,
     },
     lastName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 30,
     },
     emailID: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid");
+        }
+      },
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     age: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
+      min: 18,
+      max: 100,
     },
     gender: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      validate(value) {
+        if (!["Male", "Female", "Other"].includes(value)) {
+          throw new Error("Gender does not valid");
+        }
+      },
     },
-});
+    photoURL: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Photo URL is not valid");
+        }
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
